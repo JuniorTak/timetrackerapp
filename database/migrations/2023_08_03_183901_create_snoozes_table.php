f4553e2422f0fc9c060598be1af0d7fc973da1b1
Hyppolite <roinujtak@gmail.com>
2023-08-03 21:09:51 +0100

    Create Shift, Pause and Snooze models with migrations, and edit User model to add a relation

---
+++ database/migrations/2023_08_03_183901_create_snoozes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('snoozes', function (Blueprint $table) {
+            $table->id();
+            $table->time('snooze_on');
+            $table->time('snooze_off');
+            $table->timestamps();
+            $table->foreignId('shift_id')
+            ->constrained()
+            ->onUpdate('restrict')
+            ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('snoozes');
+    }
+};

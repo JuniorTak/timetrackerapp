86993e18a8777d1cd2d669349b6f03a80f724a99
Hyppolite <roinujtak@gmail.com>
2023-09-10 21:00:00 +0100

    Create a template structure and add a favicon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,15 +3,24 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Films</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" />
     @yield('css')
   </head>
   <body>
-    <main class="section">
-        <div class="container">
-            @yield('content')
-        </div>
-    </main>
+    <x-app-layout>
+      <div class="py-12">
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+              <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <main class="section">
+                  <div class="container">  
+                    @yield('content')
+                    </div>
+                  </main>
+              </div>
+          </div>
+      </div>
+    </x-app-layout>  
   </body>
 </html>

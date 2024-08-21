20658962a955db8cb4841c753f225685f65b48e3
hyppolite <hyppolitak@gmail.com>
2024-08-21 02:30:16 +0100

    Update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\{ Team, User, Shift };
+use App\Models\{ Team, User, Shift, Pause, Snooze };
 use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
@@ -14,32 +14,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create an admin user.
+        User::factory()->create(['is_admin' => true]);
+        
+        // Create three non-admin users.
         User::factory(3)->create();
 
-        $arraydt = array();
-        $ids = [1, 2, 3];
+        $arraydt = [];
+
         for ($i = 1; $i <= 31; $i++)
-        {
             array_push($arraydt, Carbon::createFromDate(2023, 8, $i));
-        }
+
+        $non_admin_users = User::where('is_admin', false)->get();
+
         foreach($arraydt as $dt)
         {
-            foreach($ids as $id)
+            foreach($non_admin_users as $user)
             {
-                Shift::factory()->create(['the_date' => $dt, 'user_id' => $id]);
+                Shift::factory()->create(['the_date' => $dt, 'user_id' => $user->id]);
             }
         }
 
+        $total_shifts = Shift::all()->count();
+
         $j = 1;
-        for ($i = 1; $i <= 93; $i++)
+        for ($i = 1; $i <= $total_shifts; $i++)
         {
-            // Create a pause for the corresponding shift
+            // Create a pause for the corresponding shift.
             Pause::factory()->create(['shift_id' => $i]);
 
-            // Randomly create a snooze or not for the corresponding shift
+            // Randomly create a snooze or not for the corresponding shift.
             if (rand(0,1) == 1) Snooze::factory()->create(['shift_id' => $i]);
 
-            // Create extra pauses on a basis of specific series
+            // Create extra pauses on a basis of specific series.
             if ($j == $i)
             {
                 Pause::factory()->create([

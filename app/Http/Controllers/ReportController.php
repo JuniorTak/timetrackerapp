f57273fa12d0e8afa936f3f2f6aacce7833872de
hyppolite <hyppolitak@gmail.com>
2024-08-21 01:34:20 +0100

    Implement report controller with pagination

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\{Shift, User, Pause, Snooze};
 use Illuminate\View\View;
+use Carbon\Carbon;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ReportController extends Controller
 {
@@ -15,40 +17,67 @@ class ReportController extends Controller
     public function show() : View
     {
         $users = User::get();
-        $shifts = Shift::get();
-        $activityTime = [];
+        $activityTimes = [];
         
-        // Computation of the working activity
+        // Computation of the working activity time.
         foreach($users as $user)
         {
+            $shifts = Shift::where('user_id', $user->id)->get();
             foreach($shifts as $shift)
             {
                 $pausequery = Pause::where('shift_id', $shift->id);
                 $snoozequery = Snooze::where('shift_id', $shift->id);
-                $snoozecheck = Snooze::where('shift_id', $shift->id)->exists();
+
+                // Initialize total break time and total snooze time to zero.
+                $pauseTimeInSec = 0;
+                $snoozeTimeInSec = 0;
+
                 if ($pausequery->exists())
                 {   
                     $pauses = $pausequery->get();
                     foreach ($pauses as $pause)
-                        $pauseTime[$user->id] += ($pause->pause_off - $pause->pause_in);
+                        $pauseTimeInSec += Carbon::parse($pause->pause_off)->secondsSinceMidnight() - Carbon::parse($pause->pause_on)->secondsSinceMidnight();
                 }
 
                 if ($snoozequery->exists())
                 {
                     $snoozes = $snoozequery->get();
                     foreach ($snoozes as $snooze)
-                        $snoozeTime[$user->id] += ($snooze->snooze_off - $snooze->snooze_in);
+                        $snoozeTimeInSec += Carbon::parse($snooze->snooze_off)->secondsSinceMidnight() - Carbon::parse($snooze->snooze_on)->secondsSinceMidnight();
                 }
 
-                $activityTime[$user->id] = $shift->timeOut - $shift->timeIn;
-                $activityTime['date'] = $shift->date;
+                $activityTimeInSec = Carbon::parse($shift->time_out)->secondsSinceMidnight() - Carbon::parse($shift->time_in)->secondsSinceMidnight();
+                
+                //Check if the total break time is greater than 30 min.
+                if ($pauseTimeInSec > 1800)
+                    $activityTimeInSec -= $pauseTimeInSec;
 
-                if ($pauseTime[$user->id] > '00:30:00')
-                    $activityTime[$user->id]= $activityTime[$user->id] - $pauseTime[$user->id];
-                if ($snoozeTime[$user->id] > '00:30:00')
-                    $activityTime[$user->id]= $activityTime[$user->id] - $snoozeTime[$user->id];
+                // Check if the total snooze time is greater than 30 min.
+                if ($snoozeTimeInSec > 1800)
+                    $activityTimeInSec -= $snoozeTimeInSec;
+                
+                array_push($activityTimes, [
+                    'user_id' => $user->id,
+                    'user_name' => $user->name,
+                    'shift_date' => $shift->the_date,
+                    'active_time' => gmdate('H:i:s', $activityTimeInSec) // $activityTimeInSec->format('H:i:s');
+                ]);
             }
         }
-        return view('report', compact('activityTime'));
+
+        $paginatedActivityTimes = $this->paginate($activityTimes);
+
+        return view('report', compact('paginatedActivityTimes'));
+    }
+
+    /**
+     * Paginate an array of items
+     * @return LengthAwarePaginator
+     */
+    private function paginate(array $items, int $perPage = 10, ?int $page = null, $options = []): LengthAwarePaginator
+    {
+        $page = $page ?: (LengthAwarePaginator::resolveCurrentPage() ?: 1);
+        $items = collect($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 }

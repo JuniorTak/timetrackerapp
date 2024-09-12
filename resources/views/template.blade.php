35fbc040d5ed18eb292173981a4c0a255887fa92
hyppolite <hyppolitak@gmail.com>
2024-09-12 14:38:38 +0100

    Complete laravel/breeze install and configuration

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -22,7 +22,7 @@
     </nav>
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
           <main class="section">
             <div class="container">  
               @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShiftController;
 use App\Http\Controllers\ReportController;
@@ -16,8 +17,20 @@ use App\Http\Controllers\ReportController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 // Time tracker routes
 Route::get('/shifts', [ShiftController::class, 'index'])->name('shifts.index');

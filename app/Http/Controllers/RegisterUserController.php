ae8fb8a557cfc32c4f0188c29f0cc74be32a8b7f
hyppolite <hyppolitak@gmail.com>
2024-09-13 19:17:35 +0100

    Create and implement RegisterUserController

---
+++ app/Http/Controllers/RegisterUserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class RegisterUserController extends Controller
+{
+    /**
+     * Display the user registration view.
+     */
+    public function create() : View
+    {
+        return view('auth.register-user');
+    }
+
+    /**
+     * Handle an incoming user registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request) : RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        return redirect()->back()->with('success', 'User registered successfully.');
+    }
+}

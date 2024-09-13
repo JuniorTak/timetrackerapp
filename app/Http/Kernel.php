c0ae56c08edafaebfa2122b50260c156395f80d4
hyppolite <hyppolitak@gmail.com>
2024-09-13 12:51:24 +0100

    Create and register middleware for access control

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.admin' => \App\Http\Middleware\CheckAdminRegistration::class,
+        'is.admin' => \App\Http\Middleware\IsAdmin::class,
     ];
 }

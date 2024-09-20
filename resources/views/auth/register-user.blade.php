358cd7d39116ffa20e860074d9be1e2679ad34fa
hyppolite <hyppolitak@gmail.com>
2024-09-20 18:30:34 +0100

    Update register-user auth/view to display session flash message

--- resources/views/auth/register-user.blade.php
+++ resources/views/auth/register-user.blade.php
@@ -1,7 +1,16 @@
 <x-app-layout>
-    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" style="min-height: calc(100vh - 64px);">
-        <h1 class="uppercase">{{ __('Register a new user') }}</h1>
-        
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New User') }}
+        </h2>
+    </x-slot>
+
+    @if (session('success'))
+        <div class="px-4 py-2 text-center text-white" style="background: green;">
+            {{ session('success') }}
+        </div>
+    @endif
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <form method="POST" action="{{ route('register-user') }}">
                 @csrf

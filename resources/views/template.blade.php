0b706bf82f75a43211522f1701f1865664927dc2
Hyppolite T. <hyppolitak@gmail.com>
2025-03-08 20:42:28 +0100

    Adjust styles for views template and app layout

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,6 @@
 
   <x-slot name="head">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
     @yield('css')
   </x-slot>
 
@@ -15,7 +14,7 @@
     </h2>
   </x-slot>
   
-  <div class="py-12">
+  <div class="py-4">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
         <div class="section">

0d5962b6f7e8ad9d71c1c38f236589d64b7157d6
Hyppolite T. <hyppolitak@gmail.com>
2025-03-09 23:17:14 +0100

    Add date filter to the report view and handle request query strings

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -9,6 +9,9 @@ Reports
             align-items: center;
             padding: 0.4em;
         }
+        .is-margin-left-negative {
+            margin-left: -1em;
+        }
         .select, .is-info {
             margin: 0.3em;
         }
@@ -20,33 +23,63 @@ Reports
 @section('content')
     @if (count($paginatedActivityTimes) > 0)
         <div class="card">
-            <div class="card-header">
+            <div class="card-header is-flex flex-wrap">
                 <p class="card-header-title">Reports</p>
-                <div class="select">
-                    <select onchange="window.location.href = this.value">
-                        <option value="{{ route('reports.index') }}" @unless($id) selected @endunless>All users</option>
-                        @foreach($users as $user)
-                            <option value="{{ route('reports.user', $user->id) }}" {{ $id == $user->id ? 'selected' : '' }}>{{ $user->name }}</option>
-                        @endforeach
-                    </select>
+                <!-- Filter form -->
+                <form method="GET" action="{{ url()->current() }}" class="">
+                    <div class="columns gap-6">
+                        <div class="column is-one-third is-flex is-align-items-center gap-2">
+                            <label for="start_date">From</label>
+                            <input type="date" id="start_date" name="start_date" value="{{ $startDate }}">
+                        </div>
+                        <div class="column is-one-third is-flex is-align-items-center gap-2">
+                            <label for="end_date">To</label>
+                            <input type="date" id="end_date" name="end_date" value="{{ $endDate }}">
+                        </div>
+                        <div class="column is-one-third is-flex is-align-items-center is-margin-left-negative">
+                            <button type="submit" class="button is-info">Filtrer</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="is-flex is-align-items-center gap-2 ml-2">
+                    <label for="selected-user" class="is-flex is-align-items-center">For</label>
+                    <div class="select">
+                        <select id="selected-user" class="form-select has-text-weight-semibold" onchange="window.location.href = this.value">
+                            @php
+                            $queryParams = request()->query();
+                            // Remove pagination-related parameters.
+                            unset($queryParams['page'], $queryParams['per_page']);
+                            @endphp
+                            <option value="{{ route('reports.index', $queryParams) }}" @unless($id) selected @endunless>All users</option>
+                            @foreach($users as $user)
+                                <option value="{{ route('reports.user', array_merge(['id' => $user->id], $queryParams)) }}" {{ $id == $user->id ? 'selected' : '' }}>{{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
             </div>
             <div class="card-content">
                 <div class="content">
-                <table class="table is-hoverable">
+                    <table class="table is-hoverable">
                         <thead>
                             <tr>
                                 <th>Date of shift</th>
-                                <th>User name</th>
+                                @if(!$id)<th> User name</th>@endif
                                 <th>Active time</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($paginatedActivityTimes as $activityTime)
                                 <tr>
                                     <td>{{ $activityTime['shift_date'] }}</td>
-                                    <td>{{ $activityTime['user_name'] }}</td>
+                                    @if(!$id)<td>{{ $activityTime['user_name'] }}</td>@endif
                                     <td>{{ $activityTime['active_time'] }}</td>
+                                    <td class='actions is-flex is-justify-content-center'>
+                                        <a class="button is-link is-outlined" href="#">
+                                            <span class="icon mx-2">👁️‍🗨️</span>
+                                        </a>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
@@ -54,7 +87,7 @@ Reports
                 </div>
             </div>
             <footer class="card-footer">
-                {{ $paginatedActivityTimes->withPath(url()->current())->links() }}
+                {{ $paginatedActivityTimes->withPath(url()->current())->withQueryString()->links() }}
             </footer>
         </div>
     @else

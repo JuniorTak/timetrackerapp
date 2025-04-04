717abe9e79e927daa21dda3b27ea5e038a1d9b68
Hyppolite T. <hyppolitak@gmail.com>
2025-04-04 13:58:17 +0100

    Update main app.js file to import Chart, moment and moment-timezone, make then globally available and set moment timezone.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
 
+import moment from "moment";
+import "moment-timezone";
+import Chart from 'chart.js/auto';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Make Moment and Chart globally available
+window.moment = moment;
+window.Chart = Chart;
+
+// Set moment timezone same as Laravel
+moment.tz.setDefault("UTC");
